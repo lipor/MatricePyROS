27d84d355675ad7f0802943f52499df3df9ea7a1
JustinePend <justinetulip@gmail.com>
2021-08-10 13:59:33 -0700

    add startup .sh files for boot

---
+++ catkin_ws/src/Onboard-SDK-ROS-3.2/dji_sdk_demo/dronestart.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /home/nvidia/MatricePyROS/catkin_ws
+source devel/setup.bash
+roslaunch dji_sdk sdk_manifold.launch
+
---
+++ catkin_ws/src/Onboard-SDK-ROS-3.2/dji_sdk_demo/dronestart2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash/
+cd /home/nvidia/MatricePyROS/catkin_ws
+source devel/setup.bash
+rosrun dji_sdk_demo flighttest.py
